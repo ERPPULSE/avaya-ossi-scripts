1e1634a0fd87b1403403c5f49d53b61beef6297c
SAndrii <andriyshvorak@gmail.com>
2017-08-16 16:34:34 +0300

    Create telnetconnector.py

---
+++ telnetconnector.py
@@ -0,0 +1,19 @@
+import telnetlib
+from time import sleep
+
+def connector(co):
+    comm = 'c' + str(co) + '\n'
+    tn = telnetlib.Telnet('10.89.61.20', '5023')
+    tn.read_until('login'.encode())
+    tn.write('dadmin\n'.encode())
+    tn.read_until('Password'.encode())
+    tn.write('dadmin01\n'.encode())
+    tn.read_until('Pin'.encode())
+    tn.write('dadmin01\n'.encode())
+    tn.read_until('Terminal'.encode())
+    tn.write('ossi\n'.encode())
+    tn.read_until('t\n'.encode())
+    tn.write(comm.encode())
+    tn.write('t\n'.encode())
+    output = tn.read_until('t\n'.encode()).decode('utf-8')
+    return output
