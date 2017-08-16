314461ff4e5a1452280e463e6d48d0f7fecdabf8
SAndrii <andriyshvorak@gmail.com>
2017-08-16 16:39:30 +0300

    Update staffedagents.py

--- staffedagents.py
+++ staffedagents.py
@@ -1,33 +1,10 @@
-#!/usr/bin/python
-
 import telnetlib
-from time import sleep
+from telnetconnector import connector
 
-'''Login and activate OSSI'''
-
-tn = telnetlib.Telnet('10.89.61.20', '5023')
-tn.read_until('login')
-tn.write('dadmin\n')
-tn.read_until('Password')
-tn.write('dadmin01\n')
-tn.read_until('Pin')
-tn.write('dadmin01\n')
-tn.read_until('Terminal')
-tn.write('ossi\n')
-tn.read_until('t\n')
-tn.write('clist agent-loginID\n')
-tn.write('t\n')
-output = tn.read_until('t\n').encode('utf-8')
+output = connector('li age')
 
 '''Find in output data agent's stations and count how many whey are'''
 res = 0
 for i in range(5801,5822,1):
         res += output.count(str(i))
-
-'''Logoff OSSI'''
-tn.write('clogoff\n')
-tn.write('t\n')
-sleep(0.1)
-tn.write('y\n')
-
-print "Staffed agents: %s" % (res)
+print ("Staffed agents: %s" % (res))
