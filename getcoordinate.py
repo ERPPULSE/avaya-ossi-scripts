60f70d1a5b405736dc35005673e1f72f99255bef
SAndrii <andriyshvorak@gmail.com>
2017-08-16 16:49:45 +0300

    Update getcoordinate.py

--- getcoordinate.py
+++ getcoordinate.py
@@ -1,20 +1,6 @@
-import telnetlib
+from telnetconnector import connector
 
-comm = 'c' + str(input('Enter command to get field address:')) + '\n'
-tn = telnetlib.Telnet('10.89.61.20', '5023')
-tn.read_until('login'.encode())
-tn.write('dadmin\n'.encode())
-tn.read_until('Password'.encode())
-tn.write('dadmin01\n'.encode())
-tn.read_until('Pin'.encode())
-tn.write('dadmin01\n'.encode())
-tn.read_until('Terminal'.encode())
-tn.write('ossi\n'.encode())
-tn.read_until('t\n'.encode())
-tn.write(comm.encode())
-tn.write('t\n'.encode())
-output = tn.read_until('t\n'.encode()).decode('utf-8')
-
+output = connector(str(input('Enter command to get field address:')))
 fields = {}
 data = {}
 lines = output.split('\n')
