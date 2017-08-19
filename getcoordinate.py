f89cc0a8fc9e9c21cfa8e10276f9bf3c6e221741
SAndrii <andriyshvorak@gmail.com>
2017-08-19 11:28:29 +0300

    Update getcoordinate.py

--- getcoordinate.py
+++ getcoordinate.py
@@ -1,32 +1,27 @@
 from telnetconnector import connector
 
 output = connector(str(input('Enter command to get field address:')))
-fields = {}
-data = {}
-lines = output.split('\n')
-for line in lines:
-    if line.startswith('d'):
-        data.update({
-            len(data): line[1:]
-	})
-    elif line.startswith('f'):
-        fields.update({
-	    len(fields): line[1:]
-	})
-    elif line.startswith('t'):
-        break
-    else:
-        pass
-parse = {
-    'fields': fields,
-    'data': data,
-}
-result = {} 
-for i in range(len(parse['fields'])):
-    fids = parse['fields'][i].split('\t')
-    data = parse['data'][i].split('\t')
-    for i in range(len(fids)):
-        result.update({
-            fids[i]: data[i]
-        })
-print (result)
+if output[:2] == 'cc':
+	print('You can\'t use \'change\' command, \'disp\' and \'list\' available')
+else:
+	f,d = {},{}
+	lines = output.split('\n')
+	for line in lines:
+		if line.startswith('d'):
+			d.update({
+				len(d): line[1:]
+			})
+		elif line.startswith('f'):
+			f.update({
+				len(f): line[1:]
+			})
+		elif line.startswith('t'):
+			break
+		else:
+			pass
+	parse = {
+		'fields': f,
+		'data': d,
+	}
+	for key, value in parse.items():
+		print('{0}: {1}'.format(key,value))
