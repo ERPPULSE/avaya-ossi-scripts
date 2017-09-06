99c84dc2e8c3400d5627fed957db9bdf0d8fdd3b
SAndrii <andriyshvorak@gmail.com>
2017-09-06 13:51:01 +0300

    call in queue

--- callswaiting.py
+++ callswaiting.py
@@ -2,5 +2,4 @@ import re
 from telnetconnector import connector
 
 output = connector('monitor bcms skill 1')
-re.search('s\t(.*)\t', output).group(1)
 print ("Calls waiting: %s" % (re.search('s\t(.*)\t', output).group(1)))
